41d56a9be1793146c25bb0ef97abc16c70ee4b7a
FabianMac227 <A00830408@tec.mx>
2023-02-28 15:00:20 -0600

    Final

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Quantum Game of Life
 
-This project consists of a Quantum Cellular Automata based heavily on Jhon Conway's Game of Life, the process consists on a variable sized grid where each pixel represents a cell that can be either dead or alive, the process of dying and coming to life depends on the neighboring cells, so each snapshot is based on the last one following this set of rules.
+This project consists of a Quantum Cellular Automata based heavily on Jhon Conway's Game of Life, the process consists on a variable sized grid where each pixel represents a cell that can be either dead or alive, the process of dying and coming to life depends on the neighboring cells, so each snapshot is based on the last one following a certain set of rules. But this is _Quantum_ Game of life so cells can be Death *AND* Alive. The superposition of states allows for a dynamic system that is very different and much more random. Our program follows this other set of rules based on probabilities.
 
-- 1 or 2 neighbors    ->  A living cell dies off
-- 3 neighbors         ->  A death cell comes to life
-- 4 or more neighbors ->  A living cell dies off
+### Too few
+- 0 neighbors   ->  A living cell dies off     - 100% probability
+- 1 neighbor    ->  A living cell dies off     - 50 % probability
+
+### Goldilocks
+- 2 neighbors   ->  A death cell comes to life - 50 % probability
+- 3 neighbors   ->  A death cell comes to life - 100 % probability
+
+### Too many
+- 4 neighbors   ->  A living cell dies off     - 20 % probability
+- 5 neighbors   ->  A living cell dies off     - 40 % probability
+- 6 neighbors   ->  A living cell dies off     - 60 % probability
+- 7 neighbors   ->  A living cell dies off     - 80 % probability
+- 8 neighbors   ->  A living cell dies off     - 100% probability
+
+## Emerging Complexity
+
+One of the main studied qualities of the original game of life is the emergence of complexity, which is the emergence of complex behaviour from simple parts, an important detail is that one could not predict the complex behaviour from knowing the rules that govern the smaller parts.
+In this project we want to discover the change in the cellular automata's behaviour and the different dynamics that arise from allowing the natural "randomness" of quantum computing take the spotlight. Complex organized behaviour in this mondel is not perceivable because due to the probability based rules, a single initial state may lead to multiple outcomes, but the interactions between simple objects and complex dynamics as a result is there.
+
+## The repository
+
+Most 
+
--- exploration/40_40_example.py
+++ exploration/40_40_example.py
@@ -6,9 +6,9 @@ from pennylane import numpy as np
 import pygame
 
 # Define constants
-CELL_SIZE = 10 # Size of each cell in pixels
-GRID_WIDTH = 20 # Number of cells in horizontal direction
-GRID_HEIGHT = 20 # Number of cells in vertical direction
+CELL_SIZE = 7 # Size of each cell in pixels
+GRID_WIDTH = 40 # Number of cells in horizontal direction
+GRID_HEIGHT = 40 # Number of cells in vertical direction
 SCREEN_WIDTH = CELL_SIZE * GRID_WIDTH # Width of screen in pixels
 SCREEN_HEIGHT = CELL_SIZE * GRID_HEIGHT # Height of screen in pixels
 BLACK = (0, 0, 0) # Color for dead cells
@@ -46,41 +46,7 @@ def draw_grid():
                                              CELL_SIZE - 1, CELL_SIZE - 1))
 
 # Get neightbor cells by quantum sampling of 8 qubits
-'''
-def get_neighbors():
-    for i in range(GRID_HEIGHT):
-        for j in range(GRID_WIDTH):
-            if grid[i][j] == 1:
-                               
-                # Define device
-                dev = qml.device("default.qubit", wires=n_wires,shots = 1)
-                # Define QCA circuit 
-                @qml.qnode(dev)
-                def qca_circuit():
-                    for i in range(n_wires):       
-                        qml.Hadamard(wires = i) 
-                    return  [qml.sample(qml.PauliZ(i)) for i in range(n_wires)]
-                
-                
-                output = np.array(qca_circuit())
-                M = (1 - output) / 2 #States of the adyascent pixels
-                count = 0
-                for di in [-1, 0 ,1]: 
-                    for dj in [-1 ,0 ,1]:
-                        if di == 0 and dj == 0: # Skip self
-                            continue 
-                        ni = i + di # Neighbor row index 
-                        nj = j + dj # Neighbor column index 
-                        
-                        if ni < 0 or ni >= GRID_HEIGHT or nj < 0 or nj >= GRID_WIDTH: # Skip out-of-bounds cells 
-                            continue 
-                        if grid[ni][nj] == 1:
-                            count += 1
-                            continue
-                        grid[ni][nj] == M[count][0]     #assign state to neighbors
-                        
-                        count += 1
-'''
+
 dev = qml.device("default.qubit", wires= 12, shots = shots)
 @qml.qnode(dev)
 def circuit(x):
@@ -139,46 +105,6 @@ def quantum_update():
             new_row.append(cell_state)
         new_grid.append(new_row)
     grid = new_grid     #Updated grid
-    
-
-'''
-# Define function to count neighbors of a cell
-def count_neighbors(i, j):
-    """Count neighbors of a cell at position (i,j)"""
-    count = 0 
-    for di in [-1, 0 ,1]: 
-        for dj in [-1 ,0 ,1]:
-            if di == 0 and dj == 0: # Skip self 
-                continue 
-            ni = i + di # Neighbor row index 
-            nj = j + dj # Neighbor column index 
-            if ni < 0 or ni >= GRID_HEIGHT or nj < 0 or nj >= GRID_WIDTH: # Skip out-of-bounds cells 
-                continue 
-            if grid[ni][nj] == 1: # If neighbor is alive 
-                count += 1 # Increase count by one 
-    return count # Return count
-
-
-# Define function to update grid according to rules
-def update_grid():
-    """Update grid according to rules"""
-    global grid # Use global variable grid 
-    new_grid = [] # Create a new grid 
-    for i in range(GRID_HEIGHT):
-        new_row = []
-        for j in range(GRID_WIDTH):
-            state = grid[i][j] # Current state of cell 
-            neighbors = count_neighbors(i, j) # Number of neighbors 
-            if state == 0 and neighbors == 3: # Rule 1: Birth
-                new_state = 1 # Cell becomes alive 
-            elif state == 1 and (neighbors < 2 or neighbors > 3): # Rule 2: Death
-                new_state = 0 # Cell dies 
-            else: # Rule 3: Survival
-                new_state = state # Cell stays the same 
-            new_row.append(new_state) # Add new state to new row 
-        new_grid.append(new_row) # Add new row to new grid 
-    grid = new_grid # Replace old grid with new grid 
-'''
 
 
 # Define function to handle events
